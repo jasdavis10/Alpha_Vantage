281ef66a441b3f7544df09a9dacaa5f27d6407c9
Jason Davis <jason@Jasons-MacBook-Air.local>
2020-06-05 19:10:07 -0400

    Intraday Code

--- Testing.py
+++ Testing.py
@@ -28,7 +28,7 @@ from matplotlib.pyplot import figure
 import matplotlib.pyplot as plt
 
 # Your key here
-key = 'yourkeyhere'
+key = '1BK09VNETQVBA25U'
 # Chose your output format, or default to JSON (python dict)
 ts = TimeSeries(key, output_format='pandas')
 ti = TechIndicators(key)
@@ -42,7 +42,70 @@ aapl_sma, aapl_meta_sma = ti.get_sma(symbol='AAPL')
 
 # Visualization
 figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
-aapl_data['4. close'].plot()
+aapl_data['1. open'].plot()
 plt.tight_layout()
 plt.grid()
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+alpha_vantage_api_key = "1BK09VNETQVBA25U"
+
+def pull_intraday_time_series_alpha_vantage(alpha_vantage_api_key, ticker_name, data_interval = '1min'):
+    """
+    Pull intraday time series data by stock ticker name.
+    Args:
+        alpha_vantage_api_key: Str. Alpha Vantage API key.
+        ticker_name: Str. Ticker name that we want to pull.
+        data_interval: String. Desired data interval for the data. Can be '1min', '5min', '15min', '30min', '60min'.
+    Outputs:
+        data: Dataframe. Time series data, including open, high, low, close, and datetime values.
+        metadata: Dataframe. Metadata associated with the time series.   
+    """
+    #Generate Alpha Vantage time series object
+    ts = TimeSeries(key = alpha_vantage_api_key, output_format = 'pandas')
+    #Retrieve the data for the past sixty days (outputsize = full)
+    data, meta_data = ts.get_intraday(ticker_name, outputsize = 'full', interval= data_interval)
+    data['date_time'] = data.index
+    return data, meta_data
+
+def plot_data(df, x_variable, y_variable, title):
+    """
+    Plot the x- and y- variables against each other, where the variables are columns in
+    a pandas dataframe
+    Args:
+        df: Pandas dataframe, containing x_variable and y_variable columns. 
+        x_variable: String. Name of x-variable column
+        y_variable: String. Name of y-variable column
+        title: String. Desired title name in the plot.
+    Outputs:
+        Plot in the console. 
+    """
+    fig, ax = plt.subplots()
+    ax.plot_date(df[x_variable], 
+                 df[y_variable], marker='', linestyle='-', label=y_variable)
+    fig.autofmt_xdate()
+    plt.title(title)
+    plt.show()
+
+    
+#####################################################################################################
+#### EXECUTE IN MAIN FUNCTION #######################################################################
+ts_data, ts_metadata = pull_intraday_time_series_alpha_vantage(alpha_vantage_api_key, ticker_name = "GOOGL")
+#Plot the high prices
+plot_data(df = ts_data, 
+          x_variable = "date_time", 
+          y_variable = "2. high", 
+          title ="High Values, Google Stock, 15 Minute Data")
