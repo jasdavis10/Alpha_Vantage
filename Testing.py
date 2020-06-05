f6617b3707c4ed1c9c4868e0d9af3f10cec66435
Jason Davis <jason@Jasons-MacBook-Air.local>
2020-06-05 18:19:24 -0400

    TEST

---
+++ Testing.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 20:56:46 2020
+
+@author: jason
+"""
+
+
+from alpha_vantage.timeseries import TimeSeries
+
+# Your key here
+key = '1BK09VNETQVBA25U'
+
+ts = TimeSeries(key)
+
+aapl, meta = ts.get_daily(symbol='AAPL')
+
+#print(aapl['2019-09-12'])
+
+
+
+
+
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
+
+# Your key here
+key = 'yourkeyhere'
+# Chose your output format, or default to JSON (python dict)
+ts = TimeSeries(key, output_format='pandas')
+ti = TechIndicators(key)
+
+# Get the data, returns a tuple
+# aapl_data is a pandas dataframe, aapl_meta_data is a dict
+aapl_data, aapl_meta_data = ts.get_daily(symbol='AAPL')
+# aapl_sma is a dict, aapl_meta_sma also a dict
+aapl_sma, aapl_meta_sma = ti.get_sma(symbol='AAPL')
+
+
+# Visualization
+figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
+aapl_data['4. close'].plot()
+plt.tight_layout()
+plt.grid()
+plt.show()
